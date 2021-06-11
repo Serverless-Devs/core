b9bc487a23985055070e42898e97a1bdac8b8f5b
xsahxl <xsahxl@126.com>
2021-06-11 14:00:46 +0800

    feat: 🎸 npm link => fs.ensureSymlinkSync

--- test/fixtures/load.ts
+++ test/fixtures/load.ts
@@ -1,5 +1,5 @@
 // @ts-ignore
-import { loadApplication } from '../../src/common';
+import { loadComponent } from '../../src/common';
 
 async function test() {
   // const fc = await loadComponent(
@@ -35,8 +35,8 @@ async function test() {
   // const fc = await loadApplication({ source: 'devsapp/start-malagu' });
   // const fc = await loadComponent('xsahxl/xsahxl.github.io@0.0.1');
   // const fc = await loadComponent('devsapp/springboot');
-  process.env.skipPrompt = 'true';
-  const fc = await loadApplication('devsapp/start-malagu');
+  // process.env.skipPrompt = 'true';
+  const fc = await loadComponent('devsapp/jamstack-api');
   console.log(fc);
 }
 
