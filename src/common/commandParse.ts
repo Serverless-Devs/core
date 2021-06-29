c6c606846676dbf7ac8540e2dbfa749b65cc0c93
yuankun.yk <yuankun.yk@alibaba-inc.com>
2021-06-29 13:05:57 +0800

    feat: ncc路径修复

--- src/common/commandParse.ts
+++ src/common/commandParse.ts
@@ -17,7 +17,7 @@ function commandParse(
     newArgv = minimist(argsData, opts || {})
   }else{
     try {
-      const tempResult = child_process.execSync(`${process.argv[0]} ${path.join(__dirname, '../../src/common/utils', 'args.js')} ${argsData}`)
+      const tempResult = child_process.execSync(`${process.argv[0]} ${path.join(__dirname, 'args.js')} ${argsData}`)
       const tempOutput = tempResult.toString()
       const tempArgv = tempOutput.substring(0, tempOutput.length - 1)
       newArgv = minimist(tempArgv.split(/--serverless-devs--core--parse--/g), opts || {})
