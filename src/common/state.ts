f929ac2ce682372243d2a2930f411829d28970f4
xsahxl <xsahxl@126.com>
2021-07-26 17:53:06 +0800

    fix: 🐛 s [command] -t ./${path}/s.yml 时，state 保存路径不符合预期

--- src/common/state.ts
+++ src/common/state.ts
@@ -1,18 +1,20 @@
-import { S_CURRENT_HOME } from '../libs/common';
 import { readJsonFile, writeJsonFile } from '../libs/utils';
 import path from 'path';
 import fs from 'fs-extra';
 
 export async function getState(id: any, dirPath?: string) {
-  const temp = dirPath ? path.resolve(S_CURRENT_HOME, dirPath) : S_CURRENT_HOME;
+  const spath = path.resolve(process.env.templateFile, '../.s');
+
+  const temp = dirPath ? path.resolve(spath, dirPath) : spath;
 
   const stateFilePath = path.join(temp, `${id}.json`);
   return readJsonFile(stateFilePath);
 }
 
 export async function setState(id: any, state: any, dirPath?: string) {
-  fs.ensureDirSync(S_CURRENT_HOME);
-  const temp = dirPath ? path.resolve(S_CURRENT_HOME, dirPath) : S_CURRENT_HOME;
+  const spath = path.resolve(process.env.templateFile, '../.s');
+  fs.ensureDirSync(spath);
+  const temp = dirPath ? path.resolve(spath, dirPath) : spath;
   const stateFilePath = path.join(temp, `${id}.json`);
   fs.ensureFileSync(stateFilePath);
   writeJsonFile(stateFilePath, state);
