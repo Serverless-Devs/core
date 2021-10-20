0c76ded2e55bc2a44ad8a7caea5e1f9f0bf9e9f5
xsahxl <xsahxl@126.com>
2021-10-20 17:10:08 +0800

    feat: 🎸 getContentLength timeout 5000

--- src/common/request.ts
+++ src/common/request.ts
@@ -198,7 +198,7 @@ async function getContentLength(url: string) {
     try {
       const { headers } = await got(url, {
         method: 'HEAD',
-        timeout: { request: 5000 },
+        timeout: 5000,
       });
       return parseInt(headers['content-length'], 10);
     } catch (error) {
