bc0b30cf96503ef1e97da9ff9d35baac68fe4adf
xsahxl <xsahxl@126.com>
2021-11-04 13:59:43 +0800

    feat: 🎸 #229 Logger.task 方法实现

--- src/decorator/logger.ts
+++ src/decorator/logger.ts
@@ -1,25 +1,17 @@
-import { logger } from '../logger';
+import { Logger } from '../logger';
 
-export const Logger = (context: string) => (target: any, key: string) => {
-  // @ts-ignore
-  let _val = this[key] || logger(context);
-
+export const HLogger = (context: string) => (target: any, key: string) => {
+  let _val = new Logger(context);
   const getter = function () {
     return _val;
   };
-
   const setter = function (newVal) {
     _val = newVal;
   };
-
-  // @ts-ignore
-  if (delete this[key]) {
-    Object.defineProperty(target, key, {
-      get: getter,
-      set: setter,
-      enumerable: true,
-      configurable: true,
-    });
-  }
+  Object.defineProperty(target, key, {
+    get: getter,
+    set: setter,
+    enumerable: true,
+    configurable: true,
+  });
 };
-
