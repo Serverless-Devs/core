5860de5169005fc20b1eebea0a1c4c0f557498f0
xsahxl <xsahxl@126.com>
2022-01-21 17:15:14 +0800

    feat: 🎸 load 只针对部分fc组件兼容devsapp

--- src/common/constant.ts
+++ src/common/constant.ts
@@ -1,7 +1,22 @@
-export type Registry = 'http://registry.devsapp.cn/simple' | 'https://api.github.com/repos' | 'http://registry.serverlessfans.cn/simple';
+export type Registry =
+  | 'http://registry.devsapp.cn/simple'
+  | 'https://api.github.com/repos'
+  | 'http://registry.serverlessfans.cn/simple';
 
 export enum RegistryEnum {
   github = 'https://api.github.com/repos',
   serverlessOld = 'http://registry.serverlessfans.cn/simple',
   serverless = 'http://registry.devsapp.cn/simple',
 }
+
+export const FC_COMPONENT = [
+  'domain',
+  'fc',
+  'fc-api',
+  'fc-common',
+  'fc-core',
+  'fc-default',
+  'fc-deploy',
+  'fc-info',
+  'fc-plan',
+];
