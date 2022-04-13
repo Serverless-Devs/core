a0b758e04db75dd01ed38ed3891aac5f52774f16
xsahxl <xsahxl@126.com>
2022-04-13 10:28:29 +0800

    feat: 🎸 update COMMON_VARIABLE_TYPE_REG

--- src/common/constant.ts
+++ src/common/constant.ts
@@ -27,7 +27,7 @@ export const FC_COMPONENT = [
   'oss',
 ];
 
-export const COMMON_VARIABLE_TYPE_REG = new RegExp(/\$\{([\w\.-]*)?\}/g);
+export const COMMON_VARIABLE_TYPE_REG = new RegExp(/\$\{(.*?)\}/, 'g');
 
 export const ALIYUN_CLI = '${aliyun-cli}';
 
