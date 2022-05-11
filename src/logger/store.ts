fbdd8817cdf2507d15f9b1826638b0da6cf974a2
xsahxl <xsahxl@126.com>
2022-05-11 22:10:57 +0800

    ci: 🎡 version:0.1.36-beta.12

--- src/logger/store.ts
+++
@@ -1,9 +0,0 @@
-import { WriteStream } from 'fs-extra';
-class Store {
-  createWriteStreamInstacne: WriteStream;
-  setCreateWriteStreamInstacne(val: WriteStream) {
-    this.createWriteStreamInstacne = val;
-  }
-}
-
-export default new Store();
