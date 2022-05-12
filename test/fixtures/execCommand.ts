3e13c9b42d508320c897fe0a5862f2639b3a441d
xsahxl <xsahxl@126.com>
2022-05-12 10:15:11 +0800

    test: 💍 test execCommand

--- test/fixtures/execCommand.ts
+++ test/fixtures/execCommand.ts
@@ -2,13 +2,15 @@ import { execCommand } from '../../src'
 
 (async () => {
     const data = await execCommand({
-        // syaml: '/Users/shihuali/workspace/a/fc-custom-typescript-event/s.yaml',
-        // serverName: 'helloworld',
-        syaml: '/Users/shihuali/workspace/a/start-fc-http-nodejs12/s.yaml',
-        // serverName: 'website',
-        method: 'deploy',
-        // args: 'invoke'
-        args: ['-y', '--use-local']
+        syaml: '/Users/shihuali/workspace/a/start-fc-http-nodejs14/s.yaml',
+        args: ['-y', '--use-local'],
+        env: {
+            serverless_devs_log_path: '/Users/shihuali/workspace/core/test/fs.log',
+            // serverless_devs_log_debug: 'false',
+        },
+        method: 'plan'
     })
     console.log(data)
+
+
 })()
