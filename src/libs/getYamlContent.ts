154f567c5e679f21fe88b96820fa14bca7c01254
0xff01 <898731820@qq.com>
2022-08-26 23:22:01 +0800

    chore: fix🐛 减少重复代码

--- src/libs/getYamlContent.ts
+++ src/libs/getYamlContent.ts
@@ -7,23 +7,13 @@ import yaml from 'js-yaml';
  * @description 函数内部会兼容yaml和yml文件，返回文件内容
  */
 async function getYamlContent(filePath: string) {
-  // yaml 文件
-  if (filePath.endsWith('yaml')) {
+  if (filePath.endsWith('yml') || filePath.endsWith('yaml')) {
     if (fs.existsSync(filePath)) {
       return yaml.load(fs.readFileSync(filePath, 'utf8'));
     }
-    const ymlPath = filePath.replace('.yaml', '.yml');
-    if (fs.existsSync(ymlPath)) {
-      return yaml.load(fs.readFileSync(ymlPath, 'utf8'));
-    }
-  }
-
-  // yml 文件
-  if (filePath.endsWith('yml')) {
-    if (fs.existsSync(filePath)) {
-      return yaml.load(fs.readFileSync(filePath, 'utf8'));
-    }
-    const yamlPath = filePath.replace('.yml', '.yaml');
+    const yamlPath = filePath.endsWith('yml')
+      ? filePath.replace('yml', 'yaml')
+      : filePath.replace('yaml', 'yml');
     if (fs.existsSync(yamlPath)) {
       return yaml.load(fs.readFileSync(yamlPath, 'utf8'));
     }
