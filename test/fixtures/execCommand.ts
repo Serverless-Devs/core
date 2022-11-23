aca901c90a362c112dc5004f80fc0c2aa74f9813
xsahxl <xsahxl@126.com>
2022-11-23 21:05:48 +0800

    execCommand support default_serverless_devs_auto_log

--- test/fixtures/execCommand.ts
+++ test/fixtures/execCommand.ts
@@ -2,13 +2,15 @@ import { execCommand } from '../../src'
 
 (async () => {
     const data = await execCommand({
+        
         syaml: '/Users/shihuali/workspace/a/start-fc-http-nodejs14/s.yaml',
         args: ['-y', '--use-local'],
         env: {
-            serverless_devs_log_path: '/Users/shihuali/workspace/core/test/fs.log',
+            // serverless_devs_log_path: '/Users/shihuali/workspace/core/test/fs.log',
             // serverless_devs_log_debug: 'false',
+            default_serverless_devs_auto_log: 'false',
         },
-        method: 'plan'
+        method: 'deploy'
     })
     console.log(data)
 
