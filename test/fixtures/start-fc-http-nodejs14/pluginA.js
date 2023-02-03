df2bf6b74470fc096909c684dec7b74f455ec6f2
xsahxl <xsahxl@126.com>
2023-02-03 17:03:56 +0800

    test: add global action code again
    Signed-off-by: xsahxl <xsahxl@126.com>

--- test/fixtures/start-fc-http-nodejs14/pluginA.js
+++ test/fixtures/start-fc-http-nodejs14/pluginA.js
@@ -1,5 +1,5 @@
 module.exports = async function index(inputs, args) {
-  console.log("pluginA", JSON.stringify(inputs), args);
+  // console.log("pluginA======", JSON.stringify(inputs), args);
   return {
     ...inputs,
     pluginA: "pluginA data",
